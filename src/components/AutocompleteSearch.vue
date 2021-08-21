89ee68c4b2b4638e5e907d9860fc7594d2c0c277
Dylan Silke <d.ylan.s.silke@gmail.com>
2021-08-21 15:17:28 +1000

    Adds setup props to Select2 element

--- src/components/AutocompleteSearch.vue
+++ src/components/AutocompleteSearch.vue
@@ -1,9 +1,24 @@
 <template>
-<h1>Hi</h1>
+  <Select2></Select2>
 </template>
 
 <script>
+import Select2 from "./Select2";
 export default {
+  props: {
+    registryURL: {
+      type: String,
+      default: 'https://aristotle-te-govhack-20-z09cgb.herokuapp.com'
+    },
+
+    anzscoUUID: {
+      type: String,
+      default: '3a64083b-528d-4abe-816d-ccbb739e1675'
+    }
+  },
+  components: [
+      Select2
+  ]
 
 }
 </script>
