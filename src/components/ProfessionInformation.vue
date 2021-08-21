1a31ca8140336516cbf4b16cdf1a9d99b991e219
Dylan Silke <d.ylan.s.silke@gmail.com>
2021-08-21 17:35:45 +1000

    Adds profession information component

---
+++ src/components/ProfessionInformation.vue
@@ -0,0 +1,23 @@
+<template>
+<h1>Hi</h1>
+</template>
+
+<script>
+// A component to display basic information about a particular profession
+// Needs to query a particular profession - which is a classification item, and
+// display details from the classification item
+
+export default {
+  name: "ProfessionInformation",
+  props: {
+    value: {
+      type: String,
+      default: null
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
